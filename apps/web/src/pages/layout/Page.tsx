86d2d8f6c69f4bc171ab1405b19a774291924541
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2024-12-20 11:05:51 +0000

    feat: Add Header component and integrate user navigation in layout

--- apps/web/src/pages/layout/Page.tsx
+++ apps/web/src/pages/layout/Page.tsx
@@ -1,11 +1,22 @@
+import Header from '@web/components/Header';
+import { useCurrentUser } from '@web/contexts/User';
+import { singInUrl } from '@web/router/urls';
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const Page = ({ children }: { children: React.ReactNode }) => {
+  const user = useCurrentUser();
+
+  if (!user) return <Navigate to={singInUrl} />;
+
   return (
-    <>
-      <div className="mx-auto mt-8 max-w-7xl px-6 sm:mt-6 lg:px-8">
-        {children}
-      </div>
-    </>
+    <div className="min-h-full">
+      <Header user={user} />
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {children}
+        </div>
+      </main>
+    </div>
   );
 };
